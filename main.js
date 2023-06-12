a254d10da36f3b64bdf2b59205c0780423373eba
jeyaRENUKA1234 <renukajeyaraj24@gmail.com>
2023-06-12 12:50:56 +0530

    update

---
+++ main.js
@@ -0,0 +1,34 @@
+        // let body = document.querySelector("body");
+        // let btn = document.querySelector("#burger");
+
+        // btn.addEventListener('click', function() {
+            // body.classList.toggle("line");
+        // });
+
+        // let hamburger = document.querySelector(".hamburger");
+
+      // hamburger.addEventListener("click", function() {
+          // document.querySelector("body").classList.toggle("active");
+      // });
+	  
+	  // humburger=document.querySelector(".humburger")
+	  // humburger.onclick=function(){
+		  // navBar=document.querySelector(".nav-bar");
+		  // navBar.classList.toggle("active")
+	  // }
+	  
+	  // const hamburger=document.querySelector(".humburger");
+	  // const navMenu=document.querySelector(".nav-menu");
+	  
+	  // hamburger.addEventListener("click",()=>{
+		  // hamburger.classList.toggle("active");
+		  // navMenu.classList.toggle("active");
+	  // })
+	  // document.querySelector(".nav-link").forEach(n=>n.
+	  // addEventListener("click",()=>{
+		  // humburger.classList.remove("active");
+		  // navMenu.classList.remove("acyive");
+	  // }))
+	  
+	  
+	  
